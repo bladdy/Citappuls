55c001dd5c71ee05aea4c3aa91942676001584d7
Bladimir Almanzar <bladdy34@gmail.com>
2022-08-27 12:08:50 -0400

    combosHelpers

---
+++ Citappuls/Citappuls/Helpers/CombosHelper.cs
@@ -0,0 +1,128 @@
+﻿using Citappuls.Data;
+using Citappuls.Data.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Citappuls.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<SelectListItem>> GetComboCitiesAsync(int stateId)
+        {
+            List<SelectListItem> list = await _context.Cities
+                .Where(x => x.State.Id == stateId)
+                .Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = $"{x.Id}"
+                })
+                .OrderBy(x => x.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una ciudad...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboCountriesAsync()
+        {
+            List<SelectListItem> list = await _context.Countries.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un país...]",
+                Value = "0"
+            });
+
+            return list;
+
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboSpecialitesAsync()
+        {
+            List<SelectListItem> list = await _context.Specialties.Select(c => new SelectListItem
+            {
+                Text = c.Name,
+                Value = $"{c.Id}"
+
+            })
+                .OrderBy(c => c.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una Especialidad...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboSpecialitesAsync(IEnumerable<Speciality> filter)
+        {
+            List<Speciality> categories = await _context.Specialties.ToListAsync();
+            List<Speciality> categoriesFiltered = new();
+            foreach (Speciality category in categories)
+            {
+                if (!filter.Any(c => c.Id == category.Id))
+                {
+                    categoriesFiltered.Add(category);
+                }
+            }
+            List<SelectListItem> list = categoriesFiltered.Select(c => new SelectListItem
+            {
+                Text = c.Name,
+                Value = $"{c.Id}"
+
+            })
+                .OrderBy(c => c.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una categoría...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboStatesAsync(int countryId)
+        {
+            List<SelectListItem> list = await _context.States
+               .Where(x => x.Country.Id == countryId)
+               .Select(x => new SelectListItem
+               {
+                   Text = x.Name,
+                   Value = $"{x.Id}"
+               })
+               .OrderBy(x => x.Text)
+               .ToListAsync();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un estado/provincia...]",
+                Value = "0"
+            });
+
+            return list;
+
+        }
+    }
+}
---
+++ Citappuls/Citappuls/Helpers/ICombosHelper.cs
@@ -0,0 +1,17 @@
+﻿using Citappuls.Data.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Citappuls.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboSpecialitesAsync();
+        Task<IEnumerable<SelectListItem>> GetComboSpecialitesAsync(IEnumerable<Speciality> filter);
+
+        Task<IEnumerable<SelectListItem>> GetComboCountriesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboStatesAsync(int countryId);
+
+        Task<IEnumerable<SelectListItem>> GetComboCitiesAsync(int stateId);
+    }
+}
