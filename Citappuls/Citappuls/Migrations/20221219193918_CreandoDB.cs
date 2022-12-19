86651e75669b8225dbd78cc1ba9a3d5f2d65dfec
balmanzar <bladdy34@gmail.com>
2022-12-19 16:07:45 -0400

    Database

---
+++ Citappuls/Citappuls/Migrations/20221219193918_CreandoDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Citappuls.Migrations
+{
+    public partial class CreandoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
